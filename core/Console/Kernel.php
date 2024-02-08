0174a3095759d4a8cd593ec715611a6728332e5a
Sergey Chigrinov <meowprd@gmail.com>
2024-02-08 19:10:03 +0500

    Prepare CLI entry point

---
+++ core/Console/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PurrPHP\Console;
+
+class Kernel
+{
+
+  public function handle(): int {
+    return 0;
+  }
+}
--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use PurrPHP\Http\Kernel;
 use PurrPHP\Http\Request;
 
 /** @var \League\Container\Container $container */
-$container = require(APP_PATH . '/config/services.php');
+$container = require(CONFIG_PATH . '/services.php');
 
 $kernel = $container->get(Kernel::class);
 $kernel->handle(Request::createFromGlobals())->send();
