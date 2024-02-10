c5435f5c4d0b54de7161745a3b41d6147f1d515f
Sergey Chigrinov <meowprd@gmail.com>
2024-02-10 17:48:33 +0500

    Create session class

---
+++ core/Session/Session.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PurrPHP\Session;
+
+
+
+class Session implements SessionInterface {
+
+  private const FLASH_MESSAGES_KEY = 'flash_messages';
+
+  public function __construct() { session_start(); }
+
+  public function set(string $key, $value): void {
+    $_SESSION[$key] = $value;
+  }
+  public function setFlash(string $type, string $message): void {
+    $_SESSION[self::FLASH_MESSAGES_KEY][$type][] = $message;
+  }
+  public function get(string $key, $default = null) {
+    return $_SESSION[$key] ?? $default;
+  }
+  public function getFlash(string $type): array {
+    $flash = $_SESSION[self::FLASH_MESSAGES_KEY][$type] ?? array();
+    unset($_SESSION[self::FLASH_MESSAGES_KEY][$type]);
+    return $flash;
+  }
+  public function has(string $key): bool {
+    return isset($_SESSION[$key]);
+  }
+  public function hasFlash(string $type): bool {
+    return isset($_SESSION[self::FLASH_MESSAGES_KEY][$type]);
+  }
+  public function remove(string $key): void {
+    unset($_SESSION[$key]);
+  }
+  public function removeFlash(): void {
+    unset($_SESSION[self::FLASH_MESSAGES_KEY]);
+  }
+
+}
---
+++ core/Session/SessionInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PurrPHP\Session;
+
+interface SessionInterface {
+
+  public function set(string $key, $value): void;
+  public function setFlash(string $type, string $message): void;
+  public function get(string $key, $default = null);
+  public function getFlash(string $type): array;
+  public function has(string $key): bool;
+  public function hasFlash(string $type): bool;
+  public function remove(string $key): void;
+  public function removeFlash(): void;
+
+}
