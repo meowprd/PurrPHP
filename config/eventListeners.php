8ce7ba7776e7ee5a0079c913face9b51fc59de7c
Sergey Chigrinov <meowprd@gmail.com>
2024-02-13 22:13:43 +0500

    Cleanup project

--- config/eventListeners.php
+++ config/eventListeners.php
@@ -1,19 +1,14 @@
 <?php
 
 use PurrPHP\Event\ResponseEvent;
-use PurrPHP\App\Listeners\TestListener;
-use PurrPHP\App\Listeners\SecondListener;
-
 use PurrPHP\Event\ServiceEvent;
-use PurrPHP\App\Listeners\ServicesListener;
 
 return array(
   ResponseEvent::class => array(
-    TestListener::class,
-    SecondListener::class
+
   ),
   
   ServiceEvent::class => array(
-    ServicesListener::class
+
   )
 );
--- config/routes.php
+++ config/routes.php
@@ -3,21 +3,6 @@
 use PurrPHP\Routing\Route;
 use PurrPHP\Http\Response;
 
-use PurrPHP\App\Controllers\HomeController;
-use PurrPHP\App\Controllers\RegisterController;
-use PurrPHP\App\Controllers\LoginController;
-
-use PurrPHP\App\Middlewares\TestMiddleware;
-
 return array(
-  Route::get('/', array(HomeController::class, 'index'), array(TestMiddleware::class)),
-  
-  Route::get('/register', array(RegisterController::class, 'index')),
-  Route::post('/register', array(RegisterController::class, 'post')),
 
-  Route::get('/login', array(LoginController::class, 'index')),
-  Route::post('/login', array(LoginController::class, 'post')),
-
-  Route::get('/users/list', array(HomeController::class, 'usersList')),
-  Route::get('/test', function() { return new Response("test"); })
 );
